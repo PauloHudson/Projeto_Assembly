c206aa3d5ac79c4acc62106b1703cf16dbdd3a34
Paulo Hudson <105687302+PauloHudson@users.noreply.github.com>
2024-10-26 18:13:46 -0300

    Update main.asm
    Desisti de muito do código, para utilziar e editar os códigos prontos do professor, conseguindo assim obter sucesso

--- main.asm
+++ main.asm
@@ -1,248 +1,411 @@
-; --- Mapeamento de Hardware (8051) ---
-RS      equ     P1.3    ; Reg Select para o display LCD
-EN      equ     P1.2    ; Enable para o display LCD
-LED1    equ     P1.7    ; LED1 em P1.7
-LED2    equ     P1.6    ; LED2 em P1.6
-LED3    equ     P1.5    ; LED3 em P1.5
-LED4    equ     P1.4    ; LED4 em P1.4
 
-; Variáveis e Início
-ORG 0H
-LJMP CONFIG           ; Salta para a configuração inicial
+RS      equ     P1.3    ;Reg Select ligado em P1.3
+EN      equ     P1.2    ;Enable ligado em P1.2
 
-; Interrupção externa para iniciar o jogo
-INT_EXT0:
-    AJMP INICIAR_JOGO
-    RETI
+LED1    equ     P3.7    ; LED1 em P1.7
+LED2    equ     P3.6    ; LED2 em P1.6
+LED3    equ     P3.5    ; LED3 em P1.5
+LED4    equ     P3.4    ; LED4 em P1.4
+LED5    equ     P3.3    ; LED5 PARA P1.3
 
-; Configuração de Timer
-ORG 000Bh
-INT_TEMP0:  
-    MOV TH0, #0 
-    MOV TL0, #0 
-    RETI
+;INCIA CÓDIGO
+ACALL lcd_init
 
-;
-;
-; Lembrete para o eu do futuro: esse bloco de codigo nao esta funcionando, 
-; os leds estavam ligando pela funcao DELAY_LED e nao por aq. Se quiser testar,
-; é so pegar esse versao e deltar essas funcoes ai, que n muda nada =(.
-;
-
-; --- Funções Auxiliares ---
-; Função para acender LEDs da sequência gerada
-ACENDE_LED:
-    MOV A, R1           ; Carrega o valor da sequência gerada
-    CJNE A, #01H, PROX_LED1
-    SETB LED1            ; Acende LED 1
-	ACALL DELAY_LONGO
-	CLR LED1
-
-PROX_LED1:
-    CJNE A, #02H, PROX_LED2
-    SETB LED2            ; Acende LED 2
-	ACALL DELAY_LONGO
-	CLR LED2
-
-PROX_LED2:
-    CJNE A, #03H, PROX_LED3
-    SETB LED3            ; Acende LED 3
-	ACALL DELAY_LONGO
-	CLR LED3
-
-PROX_LED3:
-    CJNE A, #04H, FIM_LED
-    SETB LED4            ; Acende LED 4
-	ACALL DELAY_LONGO
-	CLR LED4
-
-FIM_LED:
-
-DELAY_LED:
-    ACALL DELAY_LONGO   
-    CLR LED1
-	ACALL DELAY_MAIOR 
-	SETB LED1 
-    CLR LED2
-	ACALL DELAY_MAIOR 
-	SETB LED2
-    CLR LED3
-	ACALL DELAY_MAIOR 
-	SETB LED3
-    CLR LED4
-	ACALL DELAY_MAIOR 
-	SETB LED4
-    RET
-
-; Função de delay longo para dar tempo entre os LEDs
-DELAY_LONGO:
-    MOV R2, #60000     ; Ajusta o delay
-
-DELAY_MAIOR:
-    MOV R2, #150000    ; Ajusta o delay
-
-DELAY_LONGO_LOOP:
-    DJNZ R2, DELAY_LONGO_LOOP
-    RET
+;------------------
+INICIAR_JOGO: ;main
+	ACALL GAME1
+	ACALL GAME2
+    ACALL SEQ_1
+	ACALL MOSTRA_SEQ1
+	ACALL ESPERA_ENTRADA
+	ACALL COMPARA_SEQ
+	SJMP $
+   
+;--------------------------------------
+GAME1:
+	ACALL Escreve_MSGM_INICIOGAME1
+	MOV R4, #1H	
+	ACALL DELAY_LOOP
+	RET
 	
-DELAY:
-	MOV R3, #100
-    DJNZ R3, $           ; Delay simples
-    RET
-
-; Função para exibir "GANHOU" no display LCD
-EXIBE_MSG_GANHOU:
-    ACALL CLEAR_LCD
-    MOV A, #'G'
-    ACALL SEND_CHAR_LCD
-    MOV A, #'A'
-    ACALL SEND_CHAR_LCD
-    MOV A, #'N'
-    ACALL SEND_CHAR_LCD
-    MOV A, #'H'
-    ACALL SEND_CHAR_LCD
-    MOV A, #'O'
-    ACALL SEND_CHAR_LCD
-    MOV A, #'U'
-    ACALL SEND_CHAR_LCD
-    RET
-
-; Função para exibir "ERROU" no display LCD
-EXIBE_MSG_ERROU:
-    ACALL CLEAR_LCD
-    MOV A, #'E'
-    ACALL SEND_CHAR_LCD
-    MOV A, #'R'
-    ACALL SEND_CHAR_LCD
-    MOV A, #'R'
-    ACALL SEND_CHAR_LCD
-    MOV A, #'O'
-    ACALL SEND_CHAR_LCD
-    MOV A, #'U'
-    ACALL SEND_CHAR_LCD
-    RET
-
-; --- Funções Principais ---
-INICIAR_JOGO:
-    ; Gera uma sequência aleatória
-    ACALL GERA_SEQUENCIA_ALEATORIA
-    ; Exibe a sequência para o jogador
-    MOV R0, #00H         ; Reseta contador
+GAME2:
+	ACALL Escreve_MSGM_INICIOGAME2
+	ACALL clearDisplay
+	RET
 	
-MOSTRA_SEQ:
-	ACALL DELAY_LONGO     ; Delay maior entre a exibição da sequência e a próxima interação
-    MOV R1, #30H         ; Inicializa o ponteiro
-    MOV A, @R1           ; Carrega o valor da sequência gerada (via @R1)
-    ACALL ACENDE_LED     ; Acende o LED correspondente
-    INC R1
-    CJNE R0, #04H, MOSTRA_SEQ 
 	
-    ACALL ESPERA_ENTRADA  ; Espera o jogador inserir a sequência
-    ACALL COMPARA_SEQ     ; Compara a sequência inserida
-    RET
-
-; Função para gerar sequência aleatória
-GERA_SEQUENCIA_ALEATORIA:
-    MOV R0, #04H
-    MOV R1, #30H        ; Endereço inicial para armazenar a sequência gerada
 	
-GERA_LOOP:
-	MOV A, TL0
-    MOV B, #04H         ; Mod 4 para gerar números aleatórios entre 1 e 4
-    DIV AB
-	INC B
-    MOV @R1, B          ; Armazena o valor gerado
-    INC R1
-    DJNZ R0, GERA_LOOP
-    RET
 
-; Função para comparar a sequência gerada com a entrada do usuário
 COMPARA_SEQ:
-    MOV R1, #30H        ; Endereço inicial da sequência armazenada
-    MOV R0, #40H        ; Endereço inicial da entrada do usuário
-COMPARA_LOOP:
-    MOV A, @R1
-    MOV B, @R0
-    CJNE A, B, ERRO     ; Se houver diferença, vai para ERRO
-    INC R1
-    INC R0
-    CJNE R1, #34H, COMPARA_LOOP ; Compara até 4 elementos
-    SJMP GANHOU
+	MOV R0, #30H
+	MOV R1, #40H
+	MOV R7, #5 ;limitar em apenas 5 comparações
+	
+LOOP_COMPARA:
+	MOV A, @R0
+	MOV B, @R1
+	INC R0
+	INC R1
+	CJNE A, B, ERROU
+	DJNZ R7, LOOP_COMPARA
+	ACALL ACERTOU
+	RET
+	
 
-ERRO:
-    ACALL EXIBE_MSG_ERROU
+
+ERROU:
+	ACALL ESCREVE_MSGM_ERROU
+	MOV R4, #15H	;delay maior
+	ACALL DELAY_LOOP
+	ACALL clearDisplay
+	MOV R4, #10
+	ACALL DELAY_LOOP
+	LJMP INICIAR_JOGO
+
+ACERTOU:
+	ACALL ESCREVE_MSGM_GANHOU
+	MOV R4, #15H	;delay maior
+	ACALL DELAY_LOOP
+	ACALL clearDisplay
+	MOV R4, #10
+	ACALL DELAY_LOOP
+	RET
+;----------------------------
+pisca_LED1:
+    CLR LED1            ; Acende LED 1
+	MOV R4, #05			;loop simples x5
+	ACALL DELAY_LOOP
+	SETB LED1
+	RET
+
+pisca_LED2:
+    CLR LED2            ; Acende LED 2
+	MOV R4, #05			;loop simples x5
+	ACALL DELAY_LOOP
+	SETB LED2
+	RET
+pisca_LED3:
+    CLR LED3            ; Acende LED 3
+	MOV R4, #05 		;loop simples x5
+	ACALL DELAY_LOOP
+	SETB LED3
+	RET
+pisca_LED4:
+    CLR LED4            ; Acende LED 4
+	MOV R4, #05 	;loop simples x5
+	ACALL DELAY_LOOP ;loop simples
+	SETB LED4
+	RET
+	
+pisca_LED5:
+    CLR LED5            ; Acende LED 4
+	MOV R4, #05 	;loop simples x5
+	ACALL DELAY_LOOP ;loop simples
+	SETB LED5
+	RET
+
+;estamos jogando no endereço 30. para fazer a comparação com o 40.
+SEQ_1:
+	MOV 30H, #03 
+	MOV 31H, #04
+	MOV 32H, #02
+	MOV 33H, #01
+	MOV 34H, #05
+	RET
+
+SEQ_2:
+	MOV 30H, #01
+	MOV 30H, #04
+	MOV 30H, #02
+	MOV 30H, #03
+	RET
+	
+MSGM_INICIAR_GAME1:
+  DB "ACERTE!"
+  DB 00h
+MSGM_INICIAR_GAME2:  
+  DB "A SEQUENCIA"
+  DB 00h
+ 
+MSGM_ERROU:
+  DB "PERDEU!"
+  DB 00h 
+
+MSGM_GANHOU:
+  DB "GANHOU!"
+  DB 00h 
+  
+;---------------------------
+Escreve_MSGM_GANHOU:
+	ACALL clearDisplay
+	;delayzinho
+		MOV R4, #02 	;loop simples 
+		ACALL DELAY_LOOP ;loop simples
+	;
+	MOV A, #04h
+	ACALL posicionaCursor
+	MOV DPTR,#MSGM_GANHOU           
+	ACALL escreveStringROM
+	RET
+
+Escreve_MSGM_ERROU:
+	ACALL clearDisplay
+	;delayzinho
+		MOV R4, #02 	;loop simples x5
+		ACALL DELAY_LOOP ;loop simples
+	;
+	MOV A, #04h
+	ACALL posicionaCursor
+	MOV DPTR,#MSGM_ERROU            
+	ACALL escreveStringROM
+	RET
+;---------------------------
+
+;START CODE
+Escreve_MSGM_INICIOGAME1:
+	MOV A, #04h ;CIMA 
+	ACALL posicionaCursor
+	MOV DPTR,#MSGM_INICIAR_GAME1          
+	ACALL escreveStringROM
+	RET
+	
+Escreve_MSGM_INICIOGAME2:
+	MOV A, #42h ;BAIXO
+	ACALL posicionaCursor
+	MOV DPTR,#MSGM_INICIAR_GAME2           
+	ACALL escreveStringROM
+	RET
+	
+	
+	
+
+
+;mostrar no led
+
+MOSTRA_SEQ1:
+	ACALL PISCA_LED3
+	ACALL PISCA_LED4
+	ACALL PISCA_LED2
+	ACALL PISCA_LED1
+	ACALL PISCA_LED5
     RET
+     
+	
 
-GANHOU:
-    ACALL EXIBE_MSG_GANHOU
-    RET
-
-; Função para limpar o display LCD
-CLEAR_LCD:
-    CLR RS
-    CLR P1.7
-    CLR P1.6
-    CLR P1.5
-    CLR P1.4
-    SETB EN
-    CLR EN
-    ACALL DELAY
-    RET
-
-; Função para enviar caracteres para o LCD
-SEND_CHAR_LCD:
-    SETB RS
-    MOV P1, A            ; Envia o dado para P1 (display LCD)
-    SETB EN
-    CLR EN
-    ACALL DELAY
-    RET
-
-; Função para esperar pela entrada do usuário
 ESPERA_ENTRADA:
-    MOV R0, #40H         ; Inicializa o ponteiro para a entrada do usuário
-    MOV R2, #00H         ; Reseta contador
+    MOV R0, #40H         ;Armazena no endereço de memoria 40
+    MOV R2, #00H         
 ESPERA_LOOP:
-       ; Aqui você deve implementar a lógica de captura da entrada real do usuário
     JNB P2.7, CAPTURA_TECLA_7
     JNB P2.6, CAPTURA_TECLA_6
     JNB P2.5, CAPTURA_TECLA_5
     JNB P2.4, CAPTURA_TECLA_4
+	JNB P2.3, CAPTURA_TECLA_3
     SJMP ESPERA_LOOP
 
 CAPTURA_TECLA_7:
-    MOV A, #01H          ; Valor que representa a tecla P2.7 pressionada (LED 1)
+    MOV A, #01H
+	ACALL PISCA_LED1
     SJMP ARMAZENA_TECLA
 
 CAPTURA_TECLA_6:
-    MOV A, #02H          ; Valor que representa a tecla P2.6 pressionada (LED 2)
+    MOV A, #02H  
+	ACALL PISCA_LED2
     SJMP ARMAZENA_TECLA
 
 CAPTURA_TECLA_5:
-    MOV A, #03H          ; Valor que representa a tecla P2.5 pressionada (LED 3)
+    MOV A, #03H          
+	ACALL PISCA_LED3
     SJMP ARMAZENA_TECLA
 
 CAPTURA_TECLA_4:
-    MOV A, #04H          ; Valor que representa a tecla P2.4 pressionada (LED 4)
+    MOV A, #04H          
+	ACALL PISCA_LED4
+    SJMP ARMAZENA_TECLA
+	
+CAPTURA_TECLA_3:
+    MOV A, #05H          
+	ACALL PISCA_LED5
     SJMP ARMAZENA_TECLA
 
 ARMAZENA_TECLA:
-    MOV @R0, A          ; Armazena o valor da tecla pressionada em memória
-    INC R0              ; Incrementa o ponteiro de memória
+    MOV @R0, A          
+    INC R0              ; Incrementa o ponteiro de memÃ³ria
     INC R2              ; Incrementa o contador de entradas
-    CJNE R2, #04H, ESPERA_LOOP ; Captura 4 entradas, então sai do loop
+    CJNE R2, #05H, ESPERA_LOOP ; Captura 4 entradas, entao sai do loop
     RET
 
-; Configuração inicial
-CONFIG:
-    MOV TMOD, #02H       ; Configura Timer 0 no modo 2
-    MOV TH0, #0          ; Inicia o valor do Timer
-    MOV TL0, #0
-    SETB TR0             ; Inicia o Timer
-    SETB EX0             ; Habilita a interrupção externa 0
-    SETB ET0             ; Habilita interrupção do Timer 0
-    SETB EA              ; Habilita interrupções gerais
-    SJMP $
+DELAY_LOOP:
+	ACALL delay
+    DJNZ R4, DELAY_LOOP
+    RET
+
+;--------------------------- LCD ------------------------------------------------------------
+escreveStringROM:
+  MOV R1, #00h
+
+loop:
+  MOV A, R1
+	MOVC A,@A+DPTR 	 ;lê da memória de programa
+	JZ finish		; if A is 0, then end of data has been reached - jump out of loop
+	ACALL sendCharacter	; send data in A to LCD module
+	INC R1			; point to next piece of data
+   MOV A, R1
+	JMP loop		; repeat
+finish:
+	RET
+
+lcd_init:
+
+	CLR RS			
+	CLR P1.7		; |
+	CLR P1.6		; |
+	SETB P1.5		; |
+	CLR P1.4		; | high nibble set
+
+	SETB EN		; |
+	CLR EN		; | negative edge on E
+
+	CALL delay		
+
+	SETB EN		; |
+	CLR EN		; | negative edge on E
+					; same function set high nibble sent a second time
+
+	SETB P1.7		; low nibble set (only P1.7 needed to be changed)
+
+	SETB EN		; |
+	CLR EN		; | negative edge on E
+				; function set low nibble sent
+	CALL delay		; wait for BF to clear
+
+	CLR P1.7		; |
+	CLR P1.6		; |
+	CLR P1.5		; |
+	CLR P1.4		; | high nibble set
+
+	SETB EN		; |
+	CLR EN		; | negative edge on E
+
+	SETB P1.6		; |
+	SETB P1.5		; |low nibble set
+
+	SETB EN		; |
+	CLR EN		; | negative edge on E
+
+	CALL delay		; wait for BF to clear
+
+	CLR P1.7		; |
+	CLR P1.6		; |
+	CLR P1.5		; |
+	CLR P1.4		; | high nibble set
+
+	SETB EN		; |
+	CLR EN		; | negative edge on E
+
+	SETB P1.7		; |
+	SETB P1.6		; |
+	SETB P1.5		; |
+	SETB P1.4		; | low nibble set
+
+	SETB EN		; |
+	CLR EN		; | negative edge on E
+
+	CALL delay		; wait for BF to clear
+	RET
 
 
-END
+sendCharacter:
+	SETB RS  		; setb RS - indicates that data is being sent to module
+	MOV C, ACC.7		; |
+	MOV P1.7, C			; |
+	MOV C, ACC.6		; |
+	MOV P1.6, C			; |
+	MOV C, ACC.5		; |
+	MOV P1.5, C			; |
+	MOV C, ACC.4		; |
+	MOV P1.4, C			; | high nibble set
+
+	SETB EN			; |
+	CLR EN			; | negative edge on E
+
+	MOV C, ACC.3		; |
+	MOV P1.7, C			; |
+	MOV C, ACC.2		; |
+	MOV P1.6, C			; |
+	MOV C, ACC.1		; |
+	MOV P1.5, C			; |
+	MOV C, ACC.0		; |
+	MOV P1.4, C			; | low nibble set
+
+	SETB EN			; |
+	CLR EN			; | negative edge on E
+
+	CALL delay			; wait for BF to clear
+	CALL delay			; wait for BF to clear
+	RET
+
+
+;|--------------------------------------------------------------------------------------|
+;|linha 1 | 00 | 01 | 02 | 03 | 04 |05 | 06 | 07 | 08 | 09 |0A | 0B | 0C | 0D | 0E | 0F |
+;|linha 2 | 40 | 41 | 42 | 43 | 44 |45 | 46 | 47 | 48 | 49 |4A | 4B | 4C | 4D | 4E | 4F |
+;|--------------------------------------------------------------------------------------|
+posicionaCursor:
+	CLR RS	
+	SETB P1.7		    ; |
+	MOV C, ACC.6		; |
+	MOV P1.6, C			; |
+	MOV C, ACC.5		; |
+	MOV P1.5, C			; |
+	MOV C, ACC.4		; |
+	MOV P1.4, C			; | high nibble set
+
+	SETB EN			; |
+	CLR EN			; | negative edge on E
+
+	MOV C, ACC.3		; |
+	MOV P1.7, C			; |
+	MOV C, ACC.2		; |
+	MOV P1.6, C			; |
+	MOV C, ACC.1		; |
+	MOV P1.5, C			; |
+	MOV C, ACC.0		; |
+	MOV P1.4, C			; | low nibble set
+
+	SETB EN			; |
+	CLR EN			; | negative edge on E
+
+	CALL delay			; wait for BF to clear
+	CALL delay			; wait for BF to clear
+	RET
+
+
+clearDisplay:
+	CLR RS	
+	CLR P1.7		; |
+	CLR P1.6		; |
+	CLR P1.5		; |
+	CLR P1.4		; | high nibble set
+
+	SETB EN		; |
+	CLR EN		; | negative edge on E
+
+	CLR P1.7		; |
+	CLR P1.6		; |
+	CLR P1.5		; |
+	SETB P1.4		; | low nibble set
+
+	SETB EN		; |
+	CLR EN		; | negative edge on E
+
+	MOV R6, #40
+	rotC:
+	CALL delay		; wait for BF to clear
+	DJNZ R6, rotC
+	RET
+
+;--------------------------- LCD ------------------------------------------------------------
+delay:
+	MOV R5, #100
+	DJNZ R5, $
+	RET
